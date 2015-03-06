e32e132259e7eed6dcdfd37e6f3e0fc83f4f174d
Ben Harrington <ben@harringtonweb.com>
2015-03-06 09:09:33 -0500

    fix counter when no .ruby-version available

--- jscript/js/pdfind.js
+++ jscript/js/pdfind.js
@@ -7,7 +7,8 @@ function find_version_file() {
     var start = ".";
     var cd = fso.GetAbsolutePathName(".");
     print(cd);
-    for (var i=0; i<dircount(cd); i++){
+    var count = dircount(cd);
+    for (var i=0; i<count; i++){
         if (fso.FileExists(cd + "\\.ruby-version")){
             return cd + "\\.ruby-version";
         } 
