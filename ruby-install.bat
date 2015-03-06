4882e696ede96ab98444dbc1da09d97d0be699a6
Ben Harrington <ben@harringtonweb.com>
2015-03-06 10:33:08 -0500

    fix dir structure for easier version control

--- ruby-install.bat
+++ ruby-install.bat
@@ -14,10 +14,15 @@ SET "outfile= "
 call:readINI location %1 location
 call:readINI outfile %1 filename
 rem echo.loc %location%
-rem echo.out %outfile%
+echo.out %outfile%
 
 wget --no-check-certificate -O %USERPROFILE%\.winrb\ruby\%outfile% %location%
 7z -o%USERPROFILE%\.winrb\ruby -y x %USERPROFILE%\.winrb\ruby\%outfile%
+
+set outfile=%outfile:.7z=%
+set outfile=%outfile:.zip=%
+
+ren %USERPROFILE%\.winrb\ruby\%outfile% %1
 
 :readINI 
 for /f "delims=" %%a in ('call %USERPROFILE%\.winrb\read-ini.bat %USERPROFILE%\.winrb\install.ini %~2 %~3') do (
